cde3d982c4bb9f7e24bab4399f17fb555e3b0c68
Vojtěch Kadlec <kadlec@spstrutnov.cz>
2023-09-13 11:33:58 +0200

    rozdělané přihlašování, potřeba dodělat změnu a nebo zrušení účtu + dodělat view pro přihlašování

--- Source/PruvodceProject/Controllers/prihlaseniController.cs
+++ Source/PruvodceProject/Controllers/prihlaseniController.cs
@@ -6,6 +6,12 @@ namespace PruvodceProject.Controllers
 {
     public class prihlaseniController : Controller
     {
+        DatovyKontext Databaze { get; }
+
+        public prihlaseniController(DatovyKontext databaze)
+        {
+            Databaze = databaze;
+        }
 
         [HttpGet]
         public IActionResult prihlasit()
@@ -17,6 +23,38 @@ namespace PruvodceProject.Controllers
         public IActionResult registrace()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult registrace(string prezdivka, string heslo, string heslo_znovu, string e_mail)
+        {
+            if(prezdivka == null || prezdivka.Trim().Length == 0)
+            {
+                return RedirectToAction("registrace");
+            }
+            if(heslo == null || heslo.Trim().Length == 0)
+            {
+                return RedirectToAction("registrace");
+            }
+            if(heslo_znovu == null || heslo_znovu.Trim().Length == 0)
+            {
+                return RedirectToAction("registrace");
+            }
+
+            if(heslo == heslo_znovu)
+            {
+                heslo = BCrypt.Net.BCrypt.HashPassword(heslo);
+            }
+
+            if(e_mail == null || e_mail.Trim().Length == 0)
+            {
+                return RedirectToAction("registrace");
+            }
+
+            Databaze.prihlasovaci_Udaje.Add(new prihlasovaci_udaje() { prihlas_jmeno = prezdivka, heslo = heslo,mail = e_mail});
+            Databaze.SaveChanges();
+
+            return RedirectToAction("prihlasit");
         }
     }
 }
