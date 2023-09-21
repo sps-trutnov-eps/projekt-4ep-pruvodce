5906054523508a9cc9a497e8754728e26e977eab
Patrik Malý <maly@spstrutnov.cz>
2023-09-21 21:16:24 +0200

    Přidání soubory modelů pro zbývající budovy. Přejmenování proměnných.

--- Source/PruvodceProject/wwwroot/js/skolni.js
+++ Source/PruvodceProject/wwwroot/js/skolni.js
@@ -3,19 +3,12 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { FlyControls } from 'three/addons/controls/FlyControls.js';
 
-
-
 /////Pro objekty, bez potřeby psaní scena.add(....)
 /////Tyto objetky nemají žádnou interakci s myší (tedy teď mají ale potom nebudou)
-function obd(w,h,d,x,y,z,typ,obrazek,barva,jmeno){
-  if (typ === 0){
-    var ct = new THREE.Mesh(new THREE.BoxGeometry(w,h,d), new THREE.MeshBasicMaterial({map: obrazek, color:barva}));
-  } 
-  else if (typ === 1){
-    var ct = new THREE.Mesh(
-      new THREE.SphereGeometry(w,h,d), 
-      new THREE.MeshStandardMaterial({map: obrazek, color:barva}));
-  }
+function obd(x,y,z,obrazek,barva,jmeno){
+  var ct = new THREE.Mesh(
+    new THREE.SphereGeometry(3,32,32), 
+    new THREE.MeshStandardMaterial({map: obrazek, color:barva}));
   ct.position.x = x;
   ct.position.y = y;
   ct.position.z = z;
@@ -38,28 +31,36 @@ function mysPohyb(e){
   mouse.y = -(e.clientY / window.innerHeight) * 2 +1;
 }
 
+var variace = "";
+
 function dotek(){
   raycaster.setFromCamera(mouse, kamera);
   const intersects = raycaster.intersectObjects(scena.children);
   if(intersects[0].object.name != "" && intersects[0].object.name != undefined){
-    document.getElementById("Mistnostvyber").style.display = 'block';
-    document.getElementById("ins").innerHTML = intersects[0].object.name;
-
-    // const novyMaterial = intersects[0].object.material.clone();
-    // novyMaterial.transparent = true;
-    // novyMaterial.opacity = 0.5;
-    // intersects[0].object.material = novyMaterial;
-
     
+    variace = intersects[0].object.name.substring(0,4);
+    intersects[0].object.name = intersects[0].object.name.substring(4);
+
+    if(variace == "info"){
+      //Požadavek informací ze serveru
+      InfoOMistnosti(intersects[0].object.name)
+      
+      //Vyplnění info panelu v html
+      document.getElementById("panelMistnostiPrekryvaPlatno").style.display = 'block';
+      document.getElementById("popisMistnosti").innerHTML = intersects[0].object.name;
+    }
   }
 }
 
 const loader = new GLTFLoader();
 
+//Proměnná pro univerzální načítání jakéholiv .gltf souboru
+const vybranamistnost = '/wwwroot/soubor3D/skolni101.gltf'
+
 //Načtení modelu
 loader.load(
 	// resource URL
-	'/wwwroot/soubor3D/skolni101.gltf',
+	vybranamistnost,
 
 	function ( gltf ) {
 		scena.add( gltf.scene );
@@ -67,7 +68,6 @@ loader.load(
 		gltf.scene; // THREE.Group
 		gltf.scenes; // Array<THREE.Group>
 		gltf.cameras; // Array<THREE.Camera>
-
 	},
 	function ( xhr ) {
 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
@@ -109,25 +109,12 @@ window.addEventListener('click', dotek);
 //window.addEventListener('click', stop, true);
 window.addEventListener('mousemove', mysPohyb, false);
 
-// function stop(){
-//   pohyb.dragToLook = false;
-// }
-
-//Nad dveřmi
-const kruh = new THREE.TorusGeometry(11, 3, 16, 100, 3.2);
-const material = new THREE.MeshStandardMaterial({color: 0xFEFFD7});
-const torus = new THREE.Mesh(kruh, material);  
-torus.position.z = -11
-torus.position.y = 18
-
-
-obd(3,3,3,20,0,0,0,KrychleLogo);
-obd(3,3,3,30,0,0,0,logo);
-
-obd(3,32,32,20,10,20,1,logo,"", "inteSk1iuvdskjbhjnknbfdkjnbbfdnkjfdhkjbfd");
-obd(3,32,32,18,10,20,1,logo,"","integjbfdnkjnbfdnbfdkjbfdnkjnbfdkjnbfdkjbfdkjgsijgfdihgfdkjgfdshkjgdshkjghkjgfdshkjgdshkjgfdshgfdshgkjshkjgewshhhgeshnbfdjnbjfdnkjbfdnkjSk2");
-obd(3,32,32,8,10,20,1,logo,"", "inteSijhgkfdjnblkfdk3");
-
+obd(20,10,20,logo,"", "infoT1");
+obd(10,10,20,logo,"", "infoT2");
+obd(20,10,20,logo,"", "infoT5");
+obd(20,10,20,logo,"", "infoT6");
+obd(18,10,20,logo,"","skskskkskskss");
+obd(8,10,20,logo,"", "infoT15");
 
 scena.add(svetlo);
 scena.background = pozadi;
@@ -135,10 +122,7 @@ scena.background = pozadi;
 
 function animate(){
   requestAnimationFrame(animate);
-  // torus.rotation.x += 0.02;
-
   pohyb.update(0.5);
-  
   renderer.render(scena, kamera);
 }
 
