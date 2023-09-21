4f8b39fe283570b16e8fef52ebe915ea57e2efec
Jakub Plecháč <plechac@spstrutnov.cz>
2023-09-21 17:50:37 +0200

    Vytvořen Učebna controller

---
+++ Source/PruvodceProject/Controllers/UcebnaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PruvodceProject.Controllers
+{
+    public class UcebnaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
