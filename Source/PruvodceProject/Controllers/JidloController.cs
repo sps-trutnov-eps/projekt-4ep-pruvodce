368e6115ce0cb381c5afd56133dab11cc5ad5482
Jan Kleistner <kleistner@spstrutnov.cz>
2023-09-24 20:33:41 +0200

    Příprava na stránky s jídlem

---
+++ Source/PruvodceProject/Controllers/JidloController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PruvodceProject.Controllers
+{
+    public class JidloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Automaty()
+        {
+            return View();
+        }
+    }
+}
