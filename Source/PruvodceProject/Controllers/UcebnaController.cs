b94af46f2c7930bcfd64e652ecdc203563e76014
Jakub Plecháč <plechac@spstrutnov.cz>
2023-09-25 17:07:25 +0200

    Vytvořen controller, který předává informace o učebně

--- Source/PruvodceProject/Controllers/UcebnaController.cs
+++ Source/PruvodceProject/Controllers/UcebnaController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PruvodceProject.Data;
+using PruvodceProject.Models;
+using System.Diagnostics;
 
 namespace PruvodceProject.Controllers
 {
     public class UcebnaController : Controller
     {
+        public PruvodceData _pruvodceData;
+
         public IActionResult Index()
         {
+           
+
             return View();
+        }
+        public Ucebna UcebnaDetail(Guid id)
+        {
+            Ucebna? ucebna = _pruvodceData.Ucebna
+                .Where(u => u.Id == id)
+                .FirstOrDefault();
+
+            Debug.WriteLine(id);
+            Debug.WriteLine(ucebna);
+
+            return ucebna;
         }
     }
 }
--- Source/PruvodceProject/Data/PruvodceData.cs
+++ Source/PruvodceProject/Data/PruvodceData.cs
@@ -5,6 +5,7 @@ namespace PruvodceProject.Data
 {
     public class PruvodceData : DbContext
     {
+        public DbSet<Ucebna> Ucebna { get; set; }
         public DbSet<UserModel> PrihlasovaciUdaje { get; set; }
 
         public DbSet<UserVerify> OverovaciUdaje { get; set; }
