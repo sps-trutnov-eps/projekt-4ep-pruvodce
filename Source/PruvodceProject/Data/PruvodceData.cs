905deea2ac270cbf5865cb29da78ee927569cc12
Aleš Obr <obr@spstrutnov.cz>
2023-09-25 18:40:17 +0200

    Doplnění databáze

--- Source/PruvodceProject/Data/PruvodceData.cs
+++ Source/PruvodceProject/Data/PruvodceData.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PruvodceProject.Models;
+using System.Security.Cryptography;
 
 namespace PruvodceProject.Data
 {
@@ -9,6 +10,20 @@ namespace PruvodceProject.Data
 
         public DbSet<UserVerify> OverovaciUdaje { get; set; }
 
+        public DbSet<AutomatyModel> Automaty { get; set; }
+
+        public DbSet<BudovyModel> Budovy { get; set; }
+        
+        public DbSet<StravovaciZarizeni> StravovaciZarizeni { get; set; }
+
+        public DbSet<Ucebna> Ucebna { get; set; }
+
+
+        //Nutno doplnit vazby mezi databázemi
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="options"></param>
         public PruvodceData(DbContextOptions<PruvodceData> options) : base(options) { }
     }
 }
