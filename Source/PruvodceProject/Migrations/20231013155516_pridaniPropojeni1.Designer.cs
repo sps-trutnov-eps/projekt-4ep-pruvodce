29db0125321161b079cee1c9af7f881c73512c77
Brian <furlong@spstrutnov.cz>
2023-10-13 17:58:30 +0200

    Implementace 1:N propojení
    Ještě nevím jak to pořádně propojit v microsoft SQL. Zatím pushnu tohle a až se s tím naučím dělat tak pushnu update!

--- Source/PruvodceProject/Migrations/20231013155516_pridaniPropojeni1.Designer.cs
+++ Source/PruvodceProject/Migrations/20231013155516_pridaniPropojeni1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PruvodceProject.Data;
 
@@ -11,9 +12,11 @@ using PruvodceProject.Data;
 namespace PruvodceProject.Migrations
 {
     [DbContext(typeof(PruvodceData))]
-    partial class PruvodceDataModelSnapshot : ModelSnapshot
+    [Migration("20231013155516_pridaniPropojeni1")]
+    partial class pridaniPropojeni1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Source/PruvodceProject/Models/BudovyModel.cs
+++ Source/PruvodceProject/Models/BudovyModel.cs
@@ -10,7 +10,5 @@ namespace PruvodceProject.Models
         public string adresa { get; set; }
         [Required]
         public string kodoveOznaceni { get; set; }
-        //Napojení N:1 mezi budovou a uèebmou (Jedna uèebna je na jedné budovì, na jedné budovì je nìkolik uèeben, tj. bude to list uèeben, musí se správnì napojit
-        //Automaty N:1 jeden automat mùže být jenom na jedné budovì, ale na jedné budovì jich mùže být nìkolik (viz H59)
     }
 }
