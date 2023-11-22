00772299fe4dbf0402940dc59283422e1228dd7a
Jakub Plecháč <plechac@spstrutnov.cz>
2023-11-22 17:22:37 +0100

    Vytvořen kontroler NahlasitProblem

--- Source/PruvodceProject/Controllers/CrowdSource.cs
+++ Source/PruvodceProject/Controllers/CrowdSource.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PruvodceProject.Data;
 using PruvodceProject.Models;
+using System.Diagnostics;
 
 namespace PruvodceProject.Controllers
 {
@@ -12,10 +13,43 @@ namespace PruvodceProject.Controllers
         {
             _databaze = databaze;
         }
+
+
         [HttpGet]
         public IActionResult NahlasitProblem()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult NahlasitProblem(string title, string text)
+        {
+            bool uzivatelNezadalNadpis = title == null || title.Trim().Length == 0;
+            bool uzivatelNezadalText = text == null || text.Trim().Length == 0;
+
+            if (uzivatelNezadalNadpis || uzivatelNezadalText)
+                return RedirectToAction("Pridat");
+
+
+            string userMail = HttpContext.Session.GetString("mail");
+
+            UserModel? user = _databaze.PrihlasovaciUdaje
+                .Where(u => u.mail == userMail)
+                .FirstOrDefault();
+
+            Guid userID = user.ID;
+
+            CrowdSourceModel problem = new CrowdSourceModel()
+            {
+                IDUzivatele = userID,
+                nadpis = title,
+                Text = text,
+                stav = "čeká na vyřízení"
+            };
+
+            _databaze.Add(problem);
+            _databaze.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
     }
-}
+}   
