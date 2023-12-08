3bc6d4e7d690815323542f81905af3c2e7b60ffc
Brian <theinappropriateword@gmail.com>
2023-12-08 22:32:27 +0100

    Správa uživatelů

--- Source/PruvodceProject/Controllers/AdminDashboardController.cs
+++ Source/PruvodceProject/Controllers/AdminDashboardController.cs
@@ -24,13 +24,74 @@ namespace PruvodceProject.Controllers
         [HttpGet]
         public IActionResult CrowdSource()
         {
-            List<CrowdSourceModel>? crowdSource = _databaze.CrowdSource
-                .ToList();
+            List<CrowdSourceModel>? crowdSource = _databaze.CrowdSource.ToList();
 
             if (HttpContext.Session.GetString("jeAdmin") == "True")
                 return View(crowdSource);
             else
                 return RedirectToAction("Prihlasit", new { chyba = "Nejste přihlášen jako admin!" });
-        } 
+        }
+
+        [HttpGet]
+        public IActionResult SpravovatUzivatele() {
+            List<UserModel> uzivatelskeData = _databaze.PrihlasovaciUdaje.ToList();
+
+            if (HttpContext.Session.GetString("jeAdmin") == "True")
+                return View(uzivatelskeData);
+            else
+                return RedirectToAction("Prihlasit", new { chyba = "Nejste přihlášen jako admin!" });
+
+        }
+        [HttpGet]
+        public IActionResult DeleteUzivatele(Guid ID)
+        {
+            if (HttpContext.Session.GetString("jeAdmin") != "True")
+                return RedirectToAction("Prihlasit", new { chyba = "Nejste přihlášen jako admin!" });
+
+            UserModel? uzivatel = _databaze.PrihlasovaciUdaje.FirstOrDefault(n => n != null && n.ID == ID);
+
+            if (uzivatel == null)
+            {
+                _databaze.PrihlasovaciUdaje.Remove(uzivatel);
+                _databaze.SaveChanges();
+            }
+
+            return RedirectToAction("SpravovatUzivatele");
+        }
+        [HttpGet]
+        public IActionResult UdelatUzivateleAdminem(Guid ID)
+        {
+            if (HttpContext.Session.GetString("jeAdmin") != "True")
+                return RedirectToAction("Prihlasit", new { chyba = "Nejste přihlášen jako admin!" });
+            UserModel? uzivatel = _databaze.PrihlasovaciUdaje.FirstOrDefault(n => n != null && n.ID == ID);
+            
+            if (uzivatel != null)
+            {
+                uzivatel.jeAdmin = true;
+                _databaze.Entry(uzivatel).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                _databaze.SaveChanges();
+            }
+
+
+            return RedirectToAction("SpravovatUzivatele");
+        }
+        [HttpGet]
+        public IActionResult OdebratUzivateleAdmina(Guid ID)
+        {
+            if (HttpContext.Session.GetString("jeAdmin") != "True")
+                return RedirectToAction("Prihlasit", new { chyba = "Nejste přihlášen jako admin!" });
+
+            UserModel? uzivatel = _databaze.PrihlasovaciUdaje.FirstOrDefault(n => n != null && n.ID == ID);
+            if (uzivatel != null)
+            {
+                uzivatel.jeAdmin = false;
+
+                _databaze.Entry(uzivatel).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                _databaze.SaveChanges();
+            }
+
+
+            return RedirectToAction("SpravovatUzivatele");
+        }
     }
 }
