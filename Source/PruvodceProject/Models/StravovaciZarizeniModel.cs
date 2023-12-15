b26b755003546f2b75d9f8661e84527b7b4c2826
Aleš Obr <obr@spstrutnov.cz>
2023-12-15 23:15:14 +0100

    Opravy drobných chyb a doplnění migrace

--- Source/PruvodceProject/Models/StravovaciZarizeniModel.cs
+++ Source/PruvodceProject/Models/StravovaciZarizeniModel.cs
@@ -10,9 +10,9 @@ namespace PruvodceProject.Models
         public string Nazev { get; set; }
         [Required]
         public string Adresa { get; set; }
-        [Required]
         public string OdkazNaMenu { get; set; }
         [Required]
         public string Popis { get; set; }
+        public string Typ {  get; set; }
     }
 }
